f75cb4a38637dc70aafbc8b8fc929a48c43ef80f
Joey Bernard <druid.bernard@gmail.com>
2024-12-03 12:27:14 -0400

    Initialize react-app version 0.0.5

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import './App.css'
+import {initImageFileMap, initMarkdownFileMap} from "./utils/file/fileUtils.js";
+import Sidebar from "./components/Sidebar.jsx";
+import {useEffect, useState} from "react";
+import MarkdownContent from "./components/MarkdownContent.jsx";
+import {Route, Routes} from "react-router-dom";
+import LoadingScreen from "./components/LoadingScreen.jsx";
+
+function App() {
+  const [isInitialized, setIsInitialized] = useState(false);
+  useEffect(() => {
+    async function initialize() {
+      await initMarkdownFileMap();
+      await initImageFileMap();
+      setIsInitialized(true);
+    }
+    initialize();
+  }, []);
+  
+  if (!isInitialized) {
+    return (<LoadingScreen dataTestId="init-loading-screen"/>);
+  }
+  
+  return (
+    <div className="horizontal-main-container">
+      <div className="workspace is-left-sidedock-open is-right-sidedock-open">
+        <Sidebar/>
+        <Routes>
+          <Route path={"/*"} element={<MarkdownContent/>}/>
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
