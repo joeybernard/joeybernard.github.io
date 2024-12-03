cb9e45910109c6e242089331d7cd2e215472c529
Joey Bernard <druid.bernard@gmail.com>
2024-12-03 13:00:05 -0400

    Cleaning out blog

--- src/components/sidebar/SidebarEmptyState.jsx
+++
@@ -1,9 +0,0 @@
-const SidebarEmptyState = () => {
-  return (
-    <div className="workspace-sidedock-empty-state" style={{display: "none"}}>
-      <p className="u-muted">아직 내용이 없습니다.</p>
-    </div>
-  )
-};
-
-export default SidebarEmptyState;
--- src/utils/parser/rehypeObsidian.js
+++
@@ -1,9 +0,0 @@
-import rehypeCallout from "./rehypeCallout.js";
-
-const rehypeObsidian = (options = {}) => {
-  return (tree) => {
-    rehypeCallout()(tree)
-  }
-}
-
-export default rehypeObsidian;
